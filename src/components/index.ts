c8c7e30afa595aa7715360c6ea19fbdd884252bf
Colin Huber <chuber@flyht.com>
2024-01-22 15:40:58 -0700

    feat: upcming events page and components.

---
+++ src/components/index.ts
@@ -0,0 +1,16 @@
+import DealsChart from "./home/deals-chart";
+import UpcomingEvents from "./home/upcoming-events";
+import AccordionHeaderSkeleton from "./skeleton/accordion-header";
+import KanbanColumnSkeleton from "./skeleton/kanban";
+import LatestActivitiesSkeleton from "./skeleton/latest-activities";
+import ProjectCardSkeleton from "./skeleton/project-card";
+import UpcomingEventsSkeleton from "./skeleton/upcoming-events";
+// import DashboardTotalCountCard from "./home/total-count-card";
+// import DashboardLatestActivities from "./home/latest-activities";
+
+export {
+    AccordionHeaderSkeleton, DealsChart, KanbanColumnSkeleton, LatestActivitiesSkeleton, ProjectCardSkeleton, UpcomingEvents,
+    UpcomingEventsSkeleton
+};
+// DashboardTotalCountCard,
+// DashboardLatestActivities,
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
-// import {
-//     DashboardLatestActivities,
-//     DashboardTotalCountCard,
-//     DealsChart,
-//     UpcomingEvents,
-// } from "@/components";
+import {
+    //     DashboardLatestActivities,
+    //     DashboardTotalCountCard,
+    DealsChart,
+    UpcomingEvents,
+} from "@/components";
 // import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries";
 // import { DashboardTotalCountsQuery } from "@/graphql/types";
+
+import { Col, Row } from "antd";
 
 export const Home = () => {
     // const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
@@ -17,65 +19,64 @@ export const Home = () => {
     // });
 
     return (
-        <div>Home</div>
-        // <div>
-        //     <Row gutter={[32, 32]}>
-        //         <Col xs={24} sm={24} xl={8}>
-        //             <DashboardTotalCountCard
-        //                 resource="companies"
-        //                 isLoading={isLoading}
-        //                 totalCount={data?.data.companies.totalCount}
-        //             />
-        //         </Col>
-        //         <Col xs={24} sm={24} xl={8}>
-        //             <DashboardTotalCountCard
-        //                 resource="contacts"
-        //                 isLoading={isLoading}
-        //                 totalCount={data?.data.contacts.totalCount}
-        //             />
-        //         </Col>
-        //         <Col xs={24} sm={24} xl={8}>
-        //             <DashboardTotalCountCard
-        //                 resource="deals"
-        //                 isLoading={isLoading}
-        //                 totalCount={data?.data.deals.totalCount}
-        //             />
-        //         </Col>
-        //     </Row>
+        <div>
+            <Row gutter={[32, 32]}>
+                <Col xs={24} sm={24} xl={8}>
+                    {/* <DashboardTotalCountCard
+                        resource="companies"
+                        isLoading={isLoading}
+                        totalCount={data?.data.companies.totalCount}
+                    /> */}
+                </Col>
+                <Col xs={24} sm={24} xl={8}>
+                    {/* <DashboardTotalCountCard
+                        resource="contacts"
+                        isLoading={isLoading}
+                        totalCount={data?.data.contacts.totalCount}
+                    /> */}
+                </Col>
+                <Col xs={24} sm={24} xl={8}>
+                    {/* <DashboardTotalCountCard
+                        resource="deals"
+                        isLoading={isLoading}
+                        totalCount={data?.data.deals.totalCount}
+                    /> */}
+                </Col>
+            </Row>
 
-        //     <Row
-        //         gutter={[32, 32]}
-        //         style={{
-        //             marginTop: "32px",
-        //         }}
-        //     >
-        //         <Col
-        //             xs={24}
-        //             sm={24}
-        //             xl={8}
-        //             style={{
-        //                 height: "460px",
-        //             }}
-        //         >
-        //             <UpcomingEvents />
-        //         </Col>
-        //         <Col
-        //             xs={24}
-        //             sm={24}
-        //             xl={16}
-        //             style={{
-        //                 height: "460px",
-        //             }}
-        //         >
-        //             <DealsChart />
-        //         </Col>
-        //     </Row>
+            <Row
+                gutter={[32, 32]}
+                style={{
+                    marginTop: "32px",
+                }}
+            >
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={8}
+                    style={{
+                        height: "460px",
+                    }}
+                >
+                    <UpcomingEvents />
+                </Col>
+                <Col
+                    xs={24}
+                    sm={24}
+                    xl={16}
+                    style={{
+                        height: "460px",
+                    }}
+                >
+                    <DealsChart />
+                </Col>
+            </Row>
 
-        //     <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
-        //         <Col xs={24}>
-        //             <DashboardLatestActivities />
-        //         </Col>
-        //     </Row>
-        // </div>
+            <Row gutter={[32, 32]} style={{ marginTop: "32px" }}>
+                <Col xs={24}>
+                    {/* <DashboardLatestActivities /> */}
+                </Col>
+            </Row>
+        </div>
     );
 };
