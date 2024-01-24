57254ce496f3a638ccf39829a31a47c7f437b606
Colin Huber <chuber@flyht.com>
2024-01-23 20:55:57 -0700

    feat: task form components.

--- src/components/index.ts
+++ src/components/index.ts
@@ -12,3 +12,14 @@ export {
     AccordionHeaderSkeleton, DashboardLatestActivities, DashboardTotalCountCard, DealsChart, KanbanColumnSkeleton, LatestActivitiesSkeleton, ProjectCardSkeleton, UpcomingEvents,
     UpcomingEventsSkeleton
 };
+
+// export * from './tags/user-tag';
+export * from './accordion';
+export * from "./tasks/form/description";
+export * from "./tasks/form/due-date";
+export * from "./tasks/form/header";
+export * from "./tasks/form/stage";
+export * from "./tasks/form/title";
+export * from "./tasks/form/users";
+export * from './text';
+
